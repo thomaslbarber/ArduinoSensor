c47c01ad276efc01beecfff07a61e8fac7c8ad79
thomaslbarber <thomaslbarber1@gmail.com>
2022-06-26 23:05:50 +0100

    Added main code for the sensor readings.

--- main/main.ino
+++ main/main.ino
@@ -1,4 +1,15 @@
+// Includes...
 #include "SR04.h"
+
+// Pin locations on the arduino board & sensor.
+#define TRIG_PIN 2
+#define ECHO_PIN 3
+
+// Sensor.
+SR04 sensor = SR04(ECHO_PIN,TRIG_PIN);
+
+// Current distance reading by the sensor.
+long distance;
 
 void setup() {
   // put your setup code here, to run once:
@@ -6,6 +17,6 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  // Read the current distance from the sensor.
+  distance = sensor.Distance();
 }

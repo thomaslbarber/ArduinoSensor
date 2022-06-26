481e8bc22945943732d70277b0889672836af404
thomaslbarber <thomaslbarber1@gmail.com>
2022-06-27 00:17:44 +0100

    Added defines for the pins of the display to match the sensor.

--- main/main.ino
+++ main/main.ino
@@ -2,9 +2,17 @@
 #include "SR04.h"
 #include "LiquidCrystal.h"
 
-// Pin locations on the arduino board & sensor.
+// Pin locations on the arduino board (for the sensor).
 #define TRIG_PIN 2
 #define ECHO_PIN 3
+
+// Pin locations on the arduino board (for the display).
+#define BS_PIN 7
+#define E_PIN 8
+#define D4_PIN 9
+#define D5_PIN 10
+#define D6_PIN 11
+#define D7_PIN 12
 
 // Sensor.
 SR04 sensor = SR04(ECHO_PIN,TRIG_PIN);
@@ -14,7 +22,7 @@ long distance;
 const long MAX_DISTANCE = 1185;
 
 // The display.
-LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
+LiquidCrystal lcd(BS_PIN, E_PIN, D4_PIN, D5_PIN, D6_PIN, D7_PIN);
 
 void setup() {
   // Initialise the display.
